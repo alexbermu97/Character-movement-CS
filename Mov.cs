ba55668cf81b3a3a8a795f14a3f17477288a571b
alexbermu97 <47413101+alexbermu97@users.noreply.github.com>
2019-03-03 14:24:44 +0100

    Add files via upload

---
+++ Mov.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float movSp;
+    Vector2 movDir;
+    
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    
+    void Update()
+    {
+        //Teclado
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        movDir = (h * transform.right + v * transform.up);
+        rb.velocity = movDir * movSp * Time.deltaTime;
+        //TecladoFin
+
+        //Mando
+        float hc = Input.GetAxis("HorizontalC");
+        float vc = Input.GetAxis("VerticalC");
+
+        movDir = (hc * transform.right + vc * transform.up);
+        rb.velocity = movDir * movSp * Time.deltaTime;
+        //MandoFin
+    }
+}
